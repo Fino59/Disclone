bef22af3efe19a1e1eb0aaeae8d268a7e7a50ced
Danilo Fontes <slipdan@hotmail.com>
2022-01-25 00:54:46 -0300

    Update Index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,57 @@
+import appConfig from '../config.json'
+
+function GlobalStyle() {
+    return (
+        <style global jsx>{`
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+                list-style: none;
+            }
+            body {
+                font-family: 'Open Sans', sans-serif;
+            }
+            /* App fit Height */ 
+            html, body, #__next {
+                min-height: 100vh;
+                display: flex;
+                flex: 1;
+            }
+            #__next {
+                flex: 1;
+            }
+            #__next > * {
+                flex: 1;
+            }
+            /* ./App fit Height */ 
+        `}</style>
+    )
+}
+
+function Title(props) {
+    const Tag = props.tag;
+    return (
+        <>
+            <Tag>{props.children}</Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: ${appConfig.theme.colors.neutrals['500']};
+                    font-size: 24px;
+                    font-weight: 600;
+                }
+            `}</style>
+        </>
+    );
+}
+
 function HomePage() {
     return (
-        <div>Olá mundo!!!</div>
+        <div>
+            <GlobalStyle />
+            <Title tag="h2">Boas vindas de volta!!!</Title>
+            <h2>Discord - NFL</h2>
+        </div>
     )
 }
   
